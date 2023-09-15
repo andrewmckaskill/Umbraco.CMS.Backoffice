a1706b9c0ac8a7540ddc268acb181885e4072523
Mads Rasmussen <madsr@hey.com>
2023-09-15 14:30:25 +0200

    move data type tree repository intro tree module

--- src/packages/settings/data-types/repository/manifests.ts
+++ src/packages/settings/data-types/repository/manifests.ts
@@ -1,5 +1,5 @@
 import { UmbDataTypeRepository } from './data-type.repository.js';
-import { UmbDataTypeTreeRepository } from './data-type-tree.repository.js';
+import { UmbDataTypeTreeRepository } from '../tree/data-type-tree.repository.js';
 import { UmbDataTypeItemStore } from './data-type-item.store.js';
 import { UmbDataTypeStore } from './data-type.store.js';
 import { UmbDataTypeTreeStore } from '../tree/data-type.tree.store.js';
